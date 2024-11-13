0e8aa2ff2f6d28f3036b99e1b6c42dac6750d6b9
DarienEstebanVesgaCarvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-13 17:57:13 -0500

    mainErrorHandling

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ from menu.calculateTipMenu import desing as firstOptionDesing
 from menu.divideAmountsMenu import desing as secondOptionDesing
 from menu.thanksMenu import desing as thirdOptionDesing
 
-userSelection = principalDesing()
+while True:
+    try:
+        userSelection = principalDesing()
+        print(f"La opción seleccionada es {userSelection}.")
+        
+        if userSelection == 1:
+            firstOptionDesing()
+        elif userSelection == 2:
+            secondOptionDesing()
+        elif userSelection == 3:
+            thirdOptionDesing()
+            break  
+        else:
+            print("Opción inválida. Por favor, intenta de nuevo.")
 
-print(f"La opción seleccionada es {userSelection}.")
-
-if userSelection == 1:
-    firstOptionDesing()
-elif userSelection == 2:
-    secondOptionDesing()
-elif userSelection == 3:
-    thirdOptionDesing()
-else:
-    print("Opción inválida. Por favor, intenta de nuevo.")
+    except ValueError:
+        print("Error: Debes ingresar un número válido (1-3).")
