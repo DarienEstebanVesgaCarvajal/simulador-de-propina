7508375d1fce404a87451ad3a96d9338386b8815
Darien Esteban Vesga Carvajal <182381184+DarienEstebanVesgaCarvajal@users.noreply.github.com>
2024-11-14 12:18:05 +0000

    mainMenuErrorHandling

--- menu/mainMenu.py
+++ menu/mainMenu.py
@@ -1,14 +1,26 @@
 import os
+
 def desing():
-    print(f"""
-    =============================================
-                SIMULADOR DE PROPINA
-    =============================================
-    1. Calcular propina y total a pagar
-    2. Calcular total a pagar divido entre varias personas
-    3. Salir
-    =============================================
-    """)
-    selectedOption = int(input("Por favor, elige una opción (1-3): "))
-    os.system('clear')
-    return selectedOption
+    while True:
+        try:
+            print(f"""
+            =============================================
+                        SIMULADOR DE PROPINA
+            =============================================
+            1. Calcular propina y total a pagar
+            2. Calcular total a pagar dividido entre varias personas
+            3. Salir
+            =============================================
+            """)
+            selectedOption = int(input("Por favor, elige una opción (1-3): "))
+            
+            if selectedOption in [1, 2, 3]:
+                os.system('clear')
+                return selectedOption
+            else:
+                print("Por favor, ingresa: 1, 2 o 3.")
+        
+        except ValueError:
+            print("Error: Ingresa un número válido (1-3).")
+        except KeyboardInterrupt:
+            print("\nPor favor, usa la opción '3' para salir.")
